1099c1263c1133122b820af4f8ac889e73249602
hgcassiopeia <ph.thitikarn_st@tni.ac.th>
2022-01-16 23:15:51 +0700

    variable, strings and numbers

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	// string
+
+	// strings variable declaration
 	var nameOne string = "Mario"
 	var nameTwo = "Luigi"
 	var nameThree string
@@ -14,4 +15,28 @@ func main() {
 	nameThree = "Zoro"
 
 	fmt.Println(nameOne, nameTwo, nameThree)
+
+	nameFour := "Yoshi" //short hand version
+	fmt.Println(nameFour)
+
+	// int variable declaration
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits & memory
+	var numOne int8 = 25
+	var numTwo int8 = -128
+	var numThree uint8 = 25 // can't assign (-) because this type start with 0
+	numFour := 10
+
+	fmt.Println(numOne, numTwo, numThree, numFour)
+
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 888888888.7
+	scoreThree := 1.5
+
+	fmt.Println(scoreOne, scoreTwo, scoreThree)
 }
